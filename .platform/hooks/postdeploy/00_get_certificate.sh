898b18f813ca8bd000e7f783462418cefc3aa499
mjpalm13 <mjpalm2006@gmail.com>
2024-12-10 21:06:55 -0700

    m

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo certbot -n -d team1-4.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email mjpalm2006@gmail.com
-sudo certbot -n -d turtleshelter-project.is404.net --nginx --agree-tos --email mjpalm2006@gmail.com
+#sudo certbot -n -d team1-4.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email mjpalm2006@gmail.com
+#sudo certbot -n -d turtleshelter-project.is404.net --nginx --agree-tos --email mjpalm2006@gmail.com
 #sudo certbot -n -d turtleshelterproject.thought-inc.com --nginx --agree-tos --email mjpalm2006@gmail.com
